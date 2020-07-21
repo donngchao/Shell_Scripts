111145bd9eef9dcfd229f02a468265e92c2dbcd6
root <1559273720@qq.com>
2020-07-21 22:30:18 +0800

    day0721 test

--- chaper01/echo_menu.sh
+++ chaper01/echo_menu.sh
@@ -22,3 +22,13 @@ echo -e "#\e[35m 4.查看CPU信息\e[0m                 #"
 echo -e "#\e[36m 5.查看账户信息\e[0m                #"
 echo -e "\033[42m---------------------------------\033[0m"
 echo
+echo -e "@@@@\e[32mTest\e[0m                     @@@@"
+echo -e "\033[45m#################################\033[0m"
+<<COMMENT
+This file shows the usage of echo -e.
+
+
+
+COMMENT
+
+printf "\e[32m%s\e[0m\n" "hello_world"
--- chaper01/first.sh
+++ chaper01/first.sh
@@ -8,3 +8,10 @@ COMMENT
 
 #echo命令可以会先一行字符串，echo后给什么字串屏幕将回显什么字串
 echo "hello the world"
+echo "why you learn to use shell"
+
+
+<<BEGIN_OF_COMMENT
+I have to learn the usage of shell command to help me solve some problem under linux environment.
+You can follow the examples here!
+BEGIN_OF_COMMENT
